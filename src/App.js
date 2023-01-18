c4619dc174070695a1d9ca864c36353eae881e0b
Florence Bastaraud <fbastaraud@yahoo.fr>
2023-01-18 16:03:50 +0100

    fetch data from public api gutendex

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
+
+  const [books, setBooks] = useState([])
+
+
+  useEffect(() => {
+
+    async function getBooks(){
+
+      const endpoint = new URL("https://gutendex.com/books/")
+      endpoint.searchParams.set("topic", "food")
+      const response = await fetch(endpoint)
+      if(response.status !== 200) console.log("erreurrrrrr")
+      const data = await response.json()
+      setBooks(data)
+
+    }
+
+    getBooks()
+  
+  }, [])
+
+  const booksEl = books.results.map(book => (
+      <p>{book.title} written by {book.authors[0].name}</p>
+  ))
+
   return (
     <div className="container">
       <h1>Books App</h1>
+      <div>{booksEl}</div>
     </div>
   );
 }
