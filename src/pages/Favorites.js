b1d80e6ebb51cbd48e2813891c0232923c14a87b
Florence Bastaraud <fbastaraud@yahoo.fr>
2023-01-29 23:50:00 +0100

    worked on placing dating on all pages

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,8 +1,17 @@
+import { ContextBooks } from "../context/ContextBooks"
+import { useContext } from "react"
 
 function Favorites(){
+  const {favorites} = useContext(ContextBooks)
+
+  const favBooksElements = favorites.map(book => (
+    <div key={book.id}>{book.title} | {book.authors}</div>
+  ))
+
   return (
       <div className="page favorites">
         <h1>Mes Favoris</h1>
+        {favBooksElements}
       </div>
   )
 }
--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,8 +1,18 @@
+import { ContextBooks } from "../context/ContextBooks"
+import { useContext } from "react"
+
 
 function Library(){
+  const {allBooks} = useContext(ContextBooks)
+  
+  const allBooksElements = allBooks.map(book => (
+    <div key={book.id}>{book.title} | {book.authors}</div>
+  ))
+
   return (
       <div className="page library">
         <h1>Bibliotèque</h1>
+        {allBooksElements}
       </div>
   )
 }
