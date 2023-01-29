ce5fb5a8bc3d2cb7b92a222c1cca516c98fd834b
Florence Bastaraud <fbastaraud@yahoo.fr>
2023-01-29 23:38:37 +0100

    added action button to details page

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -1,15 +1,21 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { ContextBooks } from "../context/ContextBooks"
 import { useContext } from "react"
 
 function BookDetails(){
+  const navigate = useNavigate()
   const {id} = useParams()
-  const {allBooks} = useContext(ContextBooks)
+  const {allBooks, addToFavorites, favorites} = useContext(ContextBooks)
+
+  console.log(favorites);
 
   const theBookDetails = allBooks.find(book => book.id === parseInt(id))
 
+  function goBack(){
+    navigate(-1)
+  }
   
-  const {title, authors, description, image_url, rating, rating_count, review_count, num_pages, genres, Quote1} = theBookDetails
+  const {title, authors, description, image_url, rating, rating_count, num_pages, genres, Quote1} = theBookDetails
 
   return (
     <div className="bd-container">
@@ -17,7 +23,7 @@ function BookDetails(){
         <h2 className="bd-title">{title}</h2>
         <h3 className="bd-authors">{authors}</h3>
         <div className="bd-stats">
-          <span>{rating}</span><span> | {rating_count}</span><span> | {review_count}</span>
+          <span>{rating}</span><span> | {rating_count} notes</span>
         </div>
         <div className="bd-stats">
           <span>{num_pages} pages</span><span> | {genres}</span>
@@ -27,6 +33,10 @@ function BookDetails(){
       </div>
       <div className="bd-cover">
       <img src={image_url} alt={`Book cover of ${title}`}/>
+      </div>
+      <div className="actions">
+        <button onClick={() => addToFavorites(theBookDetails)}>Ajouter aux favoris</button>
+        <button onClick={goBack}>Retour</button>
       </div>
     </div>
   )
