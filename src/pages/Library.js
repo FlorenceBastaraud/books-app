8f0350657955e0b8c6ecb23e15b23a5f5c9f3a4a
Florence Bastaraud <fbastaraud@yahoo.fr>
2023-02-09 22:12:35 +0100

    Displayed books elements (library page)

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -6,13 +6,19 @@ function Library(){
   const {allBooks} = useContext(ContextBooks)
   
   const allBooksElements = allBooks.map(book => (
-    <div key={book.id}>{book.title} | {book.authors}</div>
+        <div key={book.id}>
+          <img src={book.image_url} alt={book.title}/>
+          <p>{book.title}</p>
+          <p>{book.authors}</p>
+      </div>
   ))
 
   return (
       <div className="page library">
         <h1>Bibliotèque</h1>
-        {allBooksElements}
+        <div className="library-grid">
+          {allBooksElements}
+        </div>
       </div>
   )
 }
